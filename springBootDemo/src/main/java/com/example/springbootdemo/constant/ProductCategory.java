384a7a35bcdf84612342bdd850a155d9a02e55a9
ray_lu <ray_lu@arplanet.com.tw>
2025-03-17 14:55:28 +0800

    0317

---
+++ springBootDemo/src/main/java/com/example/springbootdemo/constant/ProductCategory.java
@@ -0,0 +1,5 @@
+package com.example.springbootdemo.constant;
+
+public enum ProductCategory {
+    FOOD
+}
--- springBootDemo/src/main/java/com/example/springbootdemo/rowmapper/ProductRowMapper.java
+++ springBootDemo/src/main/java/com/example/springbootdemo/rowmapper/ProductRowMapper.java
@@ -1,5 +1,6 @@
 package com.example.springbootdemo.rowmapper;
 
+import com.example.springbootdemo.constant.ProductCategory;
 import com.example.springbootdemo.model.Product;
 import org.springframework.jdbc.core.RowMapper;
 
@@ -14,7 +15,12 @@ public class ProductRowMapper implements RowMapper<Product> {
 
         product.setProductId(rs.getInt("product_id"));
         product.setProductName(rs.getString("product_name"));
-        product.setCategory(rs.getString("category"));
+
+        String categoryStr = rs.getString("category");
+        ProductCategory category = ProductCategory.valueOf(categoryStr);
+        product.setCategory(category);
+
+//      product.setCategory(rs.getString("category"));
         product.setImageUrl(rs.getString("image_url"));
         product.setPrice(rs.getInt("price"));
         product.setStock(rs.getInt("stock"));
